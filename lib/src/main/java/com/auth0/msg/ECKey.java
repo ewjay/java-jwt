468638bef25c3cbb35717a38505029b316fda438
EJay <ejay@mgi1.com>
2018-07-30 16:55:07 -0700

    Working ECKey class

--- lib/src/main/java/com/auth0/msg/ECKey.java
+++ lib/src/main/java/com/auth0/msg/ECKey.java
@@ -1,21 +1,34 @@
 
 package com.auth0.msg;
 
+import com.auth0.jwt.exceptions.oicmsg_exceptions.DeserializationNotPossible;
 import com.auth0.jwt.exceptions.oicmsg_exceptions.HeaderError;
 import com.auth0.jwt.exceptions.oicmsg_exceptions.JWKException;
 import com.auth0.jwt.exceptions.oicmsg_exceptions.SerializationNotPossible;
 import com.auth0.jwt.exceptions.oicmsg_exceptions.ValueError;
+import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.provider.JCEECPrivateKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigInteger;
 import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.Provider;
 import java.security.interfaces.ECPrivateKey;
 import java.security.interfaces.ECPublicKey;
-import java.security.spec.*;
-import java.text.ParseException;
-import java.util.*;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.ECPoint;
+import java.security.spec.ECPrivateKeySpec;
+import java.security.spec.ECPublicKeySpec;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ECKey extends Key{
 
@@ -23,44 +36,62 @@ public class ECKey extends Key{
     private String x;
     private String y;
     private String d;
+    private static Map<String, String> NIST2SEC;
+    private static Map<String, String> SEC2NIST;
     final private static Logger logger = LoggerFactory.getLogger(ECKey.class);
+    final private static Provider BOUNCYCASTLEPROVIDER = new BouncyCastleProvider();
+    final private static org.bouncycastle.jce.spec.ECParameterSpec ECSPEC256 =
+        ECNamedCurveTable.getParameterSpec("secp256r1");
+    final private static org.bouncycastle.jce.spec.ECParameterSpec ECSPEC384 =
+        ECNamedCurveTable.getParameterSpec("secp384r1");
+    final private static org.bouncycastle.jce.spec.ECParameterSpec ECSPEP521 =
+        ECNamedCurveTable.getParameterSpec("secp521r1");
+
+
     private static Set<String> longs = new HashSet<String>(Arrays.asList("x", "y", "d"));
+
+    /**
+     *
+     * Initialize the mappings between the curve representation in
+     * Cryptography and the one used by NIST (and in RFC 7518) and vice versa
+     *
+     * Also initializes know curve parameters
+     */
+    static {
+        NIST2SEC = new HashMap<String, String>();
+        NIST2SEC.put("P-256", "secp256r1");
+        NIST2SEC.put("P-384", "secp384r1");
+        NIST2SEC.put("P-521", "secp521r1");
+        NIST2SEC = new HashMap<String, String>();
+        SEC2NIST.put("secp256r1", "P-256");
+        SEC2NIST.put("secp394r1", "P-384");
+        SEC2NIST.put("secp521r1", "P-521");
+    }
 
     public ECKey(String alg, String use, String kid, java.security.Key key, String crv, String x,
                  String y, String d,
-                 Map<String,String> args) throws HeaderError, JWKException, ValueError {
+                 Map<String,String> args)
+        throws HeaderError, JWKException, ValueError, SerializationNotPossible {
         super("EC", alg, use, kid, null, "", "", key, args);
-        this.crv = crv;
-        this.x = x;
-        this.y = y;
-        this.d = d;
+        if(key == null && !NIST2SEC.containsKey(crv)) {
+            throw new ValueError("Invalid curve parameter");
+        }
+        this.crv = Utils.isNullOrEmpty(crv) ? "" : crv;
+        this.x = Utils.isNullOrEmpty(x) ? "" : x;
+        this.y = Utils.isNullOrEmpty(y) ? "" : y;
+        this.d = Utils.isNullOrEmpty(d) ? "" : d;
         members.addAll(Arrays.asList("x", "y", "d", "crv"));
         publicMembers.addAll(Arrays.asList("x", "y", "crv"));
         required.addAll(Arrays.asList("x", "y", "key", "crv"));
         if(this.key != null) {
             loadKey(this.key);
-        } else if (!Utils.isNullOrEmpty(this.x) && !Utils.isNullOrEmpty(this.y)) {
+        } else if (!Utils.isNullOrEmpty(this.x) && !Utils.isNullOrEmpty(this.y) &&
+            !Utils.isNullOrEmpty(this.crv)) {
             verify();
             deserialize();
         } else
             throw new JWKException("Missing required parameter");
-
-        if(!Utils.isNullOrEmpty(this.crv)) {
-            try {
-                this.verify();
-            } catch (HeaderError headerError) {
-                headerError.printStackTrace();
-            }
-            this.deserialize();
-        } else if(this.getKey(false) != null ) {
-            // TODO check getKey() in original
-            this.loadKey(key);
-        }
-    }
-
-//    public ECKey() {
-//        this("", "", "", null, "", null, null, null, null);
-//    }
+     }
 
     /**
      * Checks whether this is a private key
@@ -69,104 +100,123 @@ public class ECKey extends Key{
      */
     @Override
     public boolean isPrivateKey() {
-        return false;
+        if(key != null) {
+            return (key instanceof  ECPrivateKey);
+        } else {
+            return !Utils.isNullOrEmpty(d);
+        }
     }
 
-    public void deserialize() {
-//        try {
-//            if(!(this.x instanceof Number)) {
-//                this.x = deser(this.x);
-//            }
-//            if(!(this.y instanceof Number)) {
-//                this.y = deser(this.y);
-//            }
-//        } catch (ParseException e) {
-//            logger.error("Couldn't parse value");
-//        }
-//
-//        this.curve = byName(this.crv);
-//        if(this.d != null) {
-//            if(this.d instanceof String) {
-//                this.d = deser(this.d);
-//            }
-//        }
-        // TODO
-    }
+    public void deserialize() throws DeserializationNotPossible {
+        try {
+            ECParameterSpec ecParameterSpec = getECParameterSpec(crv);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            if(!Utils.isNullOrEmpty(d)) {
+                ECPrivateKeySpec ecPrivateKeySpec =
+                    new ECPrivateKeySpec(Utils.base64urlToBigInt(d), ecParameterSpec);
+                key = keyFactory.generatePrivate(ecPrivateKeySpec);
+            } else if(!Utils.isNullOrEmpty(x) && !Utils.isNullOrEmpty(y)) {
+                ECPoint publicPoint =
+                    new ECPoint(Utils.base64urlToBigInt(x), Utils.base64urlToBigInt(y));
+                ECPublicKeySpec ecPublicKeySpec = new ECPublicKeySpec(publicPoint, ecParameterSpec);
+                key = keyFactory.generatePrivate(ecPublicKeySpec);
+            }
+
+        } catch(ValueError | GeneralSecurityException e) {
+            throw new DeserializationNotPossible(e.toString());
+        }
+     }
 
     @Override
     public java.security.Key getKey(Boolean isPrivate) throws ValueError {
-        return null;
+        if(key == null) {
+            try {
+                deserialize();
+            } catch (DeserializationNotPossible e) {
+                throw new ValueError(e.toString());
+            }
+        }
+        if(isPrivate) {
+            if(!isPrivateKey()) {
+                throw new ValueError("Not a private key");
+            }
+        } else {
+            if(isPrivateKey()) {
+                try {
+                    return (java.security.Key) getPublicKeyFromPrivateKey((ECPrivateKey) key);
+                } catch(Exception e) {
+                    throw new ValueError(e.toString());
+                }
+            }
+        }
+        return key;
     }
 
-//    private EllipticCurve byName(String name) {
-//        if(name.equals("P-256")) {
-//            return EllipticCurve();
-//        } else if(name.equals("P-384")) {
-//            return EllipticCurve();
-//        } else if(name.equals("P-521")) {
-//            return EllipticCurve();
-//        }
-//    }
-//
-//    public List<Object> getKey(boolean isPrivate) {
-//        if(isPrivate) {
-//            return new ArrayList<>(Arrays.asList(this.d));
-//        } else {
-//            return new ArrayList<>(Arrays.asList(this.x, this.y));
-//        }
-//    }
-
-//    public Object serialize(boolean isPrivate) throws SerializationNotPossible {
-//        if(this.crv == null && this.curve == null) {
-//            throw new SerializationNotPossible();
-//        }
-//
-//        Map<String, String> args = common();
-//        args.put("crv", this.curve.getClass().getName());
-//        args.put("x", longToBase64(this.x));
-//        args.put("y", longToBase64(this.y));
-//
-//        if(isPrivate && this.d != null) {
-//            args.put("d", longToBase64(this.d));
-//        }
-//
-//
-//        return args;
-//    }
-
-
+    /**
+     * Go from a ECPrivateKey or ECPublicKey instance to a JWK representation.
+     *
+     * @param isPrivate
+     * @return
+     * @throws SerializationNotPossible
+     */
     public Map<String, Object> serialize(boolean isPrivate) throws SerializationNotPossible {
-        // TODO
-        return null;
+        if(!Utils.isNullOrEmpty(crv)) {
+            throw new SerializationNotPossible();
+        }
+        Map<String, Object> serializedObject = common();
+        serializeECKey(key);
+        serializedObject.put("crv", crv);
+        serializedObject.put("x", x);
+        serializedObject.put("y", y);
+        if(isPrivate && Utils.isNullOrEmpty(d)) {
+            throw new SerializationNotPossible();
+        } else {
+            serializedObject.put("d", d);
+        }
+        return serializedObject;
     }
 
 
-    private void serializeECKey(java.security.Key key) {
+    private void serializeECKey(java.security.Key key) throws SerializationNotPossible {
 
         if(key != null) {
             if(key instanceof  ECPrivateKey) {
                 ECPrivateKey privateKey = (ECPrivateKey) key;
                 d = Utils.bigIntToBase64url(privateKey.getS());
-                // TODO serialize public key parts (x, y)
-//                x = Utils.bigIntToBase64url(privateKey.ge)
+                try {
+                    ECPublicKey ecPublicKey = getPublicKeyFromPrivateKey(privateKey);
+                    x = Utils.bigIntToBase64url(ecPublicKey.getW().getAffineX());
+                    y = Utils.bigIntToBase64url(ecPublicKey.getW().getAffineY());
+                }
+                catch (Exception e) {
+                    throw  new SerializationNotPossible();
+                }
             } else if(key instanceof  ECPublicKey) {
-
+                ECPublicKey ecPublicKey = (ECPublicKey) key;
+                x = Utils.bigIntToBase64url(ecPublicKey.getW().getAffineX());
+                y = Utils.bigIntToBase64url(ecPublicKey.getW().getAffineY());
+            } else {
+                throw new SerializationNotPossible();
+            }
+            org.bouncycastle.jce.spec.ECParameterSpec ecParameterSpec =
+                EC5Util.convertSpec(((ECPrivateKey) key).getParams(), false);
+            if(ecParameterSpec.equals(ECSPEC256)) {
+                crv = "P-256";
+            } else if(ecParameterSpec.equals(ECSPEC384)) {
+                crv = "P-384";
+            } else if(ecParameterSpec.equals(ECSPEP521)) {
+                crv = "P-521";
+            } else {
+                throw new SerializationNotPossible();
             }
         }
-
-        if(key instanceof ECPrivateKey) {
-
-        } else if(key instanceof ECPublicKey) {
-
-        }
-//        ECGenParameterSpec genParameterSpec = new ECGenParameterSpec();
     }
 
-    public ECKey loadKey(java.security.Key key) {
+    public void loadKey(java.security.Key key) throws SerializationNotPossible  {
         serializeECKey(key);
         this.key = key;
-        return this;
     }
+
 //
 //    public List<Object> getDecryptionKey() {
 //        return this.getKey(true);
@@ -180,89 +230,58 @@ public class ECKey extends Key{
         return crv;
     }
 
-    public void setCrv(String crv) {
-        this.crv = crv;
-    }
-
-    public Object getX() {
+    public String getX() {
         return x;
     }
 
-    public void setX(String x) {
-        this.x = x;
-    }
-
-    public Object getY() {
+    public String getY() {
         return y;
     }
 
-    public void setY(String y) {
-        this.y = y;
-    }
-
-    public Object getD() {
+    public String getD() {
         return d;
     }
-
-    public void setD(String d) {
-        this.d = d;
-    }
-
-//    public String getCurve() {
-//        return curve;
-//    }
-//
-//    public void setCurve(Object curve) {
-//        this.curve = curve;
-//    }
 
     public static Logger getLogger() {
         return logger;
     }
 
-    public static Set<String> getLongs() {
-        return longs;
-    }
-
-    public static void setLongs(Set<String> longs) {
-        ECKey.longs = longs;
-    }
-
-//    public static Set<String> getMembers() {
-//        return members;
-//    }
-//
-//    public static void setMembers(Set<String> members) {
-//        ECKey.members = members;
-//    }
-//
-//    public static Set<String> getPublicMembers() {
-//        return publicMembers;
-//    }
-//
-//    public static void setPublicMembers(Set<String> publicMembers) {
-//        ECKey.publicMembers = publicMembers;
-//    }
-//
-//    public static Set<String> getRequired() {
-//        return required;
-//    }
-//
-//    public static void setRequired(Set<String> required) {
-//        ECKey.required = required;
-//    }
-
-    private ECParameterSpec getECParameterSpec(String curve) throws ValueError, java.security.GeneralSecurityException {
-        AlgorithmParameters parameters = AlgorithmParameters.getInstance("EC", "SunEC");
-        if(curve.equals("P-256")) {
+    private ECParameterSpec getECParameterSpec(String curve)
+        throws ValueError, java.security.GeneralSecurityException {
+        AlgorithmParameters parameters =
+            AlgorithmParameters.getInstance("EC", "SunEC");
+        if("P-256".equals(curve)) {
             parameters.init(new ECGenParameterSpec("secp256r1"));
-        } else if (curve.equals("P-384")) {
+        } else if ("P-384".equals(curve)) {
             parameters.init(new ECGenParameterSpec("secp384r1"));
-        } else if (curve.equals("P-521")) {
+        } else if ("P-521".equals(curve)) {
             parameters.init(new ECGenParameterSpec("secp521r1"));
         } else
             throw new ValueError("Invalid curve");
         return  parameters.getParameterSpec(ECParameterSpec.class);
+    }
+
+    private ECPublicKey getPublicKeyFromPrivateKey(ECPrivateKey ecPrivateKey) throws Exception{
+        ECPublicKey ecPublicKey = null;
+        if(ecPrivateKey != null) {
+            JCEECPrivateKey jceecPrivateKey = new JCEECPrivateKey(ecPrivateKey);
+            org.bouncycastle.jce.spec.ECParameterSpec ecParameterSpec =
+                jceecPrivateKey.getParameters();
+            org.bouncycastle.math.ec.ECPoint Q =
+                ecParameterSpec.getG().multiply(jceecPrivateKey.getD());
+            byte[] publicDerBytes = Q.getEncoded(false);
+            org.bouncycastle.math.ec.ECPoint point =
+                ecParameterSpec.getCurve().decodePoint(publicDerBytes);
+            org.bouncycastle.jce.spec.ECPublicKeySpec pubSpec =
+                new org.bouncycastle.jce.spec.ECPublicKeySpec(point, ecParameterSpec);
+            KeyFactory keyFactory = KeyFactory.getInstance("ECDSA", BOUNCYCASTLEPROVIDER);
+            ecPublicKey = (ECPublicKey) keyFactory.generatePublic(pubSpec);
+        }
+        return ecPublicKey;
+    }
+
+    private boolean isValidCurve(String curve) {
+        return NIST2SEC.containsKey(curve);
     }
 
 }
