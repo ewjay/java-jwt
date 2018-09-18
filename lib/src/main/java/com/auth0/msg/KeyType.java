18512d98e933814c697364103da2b2dba5e228d0
EJay <ejay@mgi1.com>
2018-09-18 12:29:23 -0700

    Deleted messaging classes

--- lib/src/main/java/com/auth0/msg/KeyType.java
+++
@@ -1,8 +0,0 @@
-package com.auth0.msg;
-
-/**
- * Classify the type of key for signing/encryption
- */
-public enum KeyType {
-    RSA, EC;
-}
--- lib/src/main/java/com/auth0/msg/KnownClaims.java
+++
@@ -1,7 +0,0 @@
-package com.auth0.msg;
-
-public class KnownClaims {
-    public static final String GRANT_TYPE = "grant_type";
-
-
-}
